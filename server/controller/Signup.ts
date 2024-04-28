913bf0d07de9e5c97969d86026014a272fbb2e27
BeNikk <bhattnik442@gmail.com>
2024-04-28 09:25:23 +0530

    signup aapi

--- server/controller/Signup.ts
+++ server/controller/Signup.ts
@@ -1,30 +1,38 @@
 import { Request, Response } from "express";
 import User from "../models/userModel";
 import bcrypt from 'bcryptjs';
+import generateToken from "../helpers/setCookie";
 export default async function signup(req:Request,res:Response){
     try{
         const {name,username,email,password}=req.body;
         const user=await User.findOne({$or:[{username},{email}]});
         if(user){
             res.status(400).json({message:"User already exists"});
+
         }
-        const salt=await bcrypt.genSalt(10);
-        const hashedpassword=await bcrypt.hash(password,salt);
-        const newUser=new User({
-            name,
-            username,
-            email,
-            password:hashedpassword
-        });
-        await newUser.save();
-        if(newUser){
-            res.status(200).json({
-                _id:newUser._id,
-                name:newUser.name,
-                email:newUser.email,
-                password:newUser.password,
-                username:newUser.username
+        else{
+
+            const salt=await bcrypt.genSalt(10);
+            const hashedpassword=await bcrypt.hash(password,salt);
+            const newUser=new User({
+                name,
+                username,
+                email,
+                password:hashedpassword
             });
+            await newUser.save();
+            if(newUser){
+                generateToken(newUser.username,res)
+                res.status(200).json({
+                    _id:newUser._id,
+                    name:newUser.name,
+                    email:newUser.email,
+                    password:newUser.password,
+                    username:newUser.username
+                });
+            }else{
+                res.status(400).json({message:"Invalid user data"});
+            }
         }
 
 
---
+++ server/helpers/setCookie.ts
@@ -0,0 +1,31 @@
+import { Response } from "express";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const jwtsecREt=process.env.JWT_SECRET || "ABC";
+
+export default async function generateToken(username:string,res:Response) {
+    try{
+
+    
+
+            const token=jwt.sign(username,jwtsecREt,{});
+            res.cookie("token",token,{
+                httpOnly:true,
+                maxAge:15*24*60*60*1000,
+                sameSite:"strict",
+
+            })
+            return token;
+            
+
+        
+        
+    }catch(e){
+        console.log(`error in signing ${e}`);
+    }
+
+
+    
+}
