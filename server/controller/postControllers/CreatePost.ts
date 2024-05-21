92b60ca5c31019c80fa513f2266f20197c1954a0
BeNikk <bhattnik442@gmail.com>
2024-05-21 09:45:31 +0530

    minor imp

--- server/controller/postControllers/CreatePost.ts
+++ server/controller/postControllers/CreatePost.ts
@@ -1,30 +1,37 @@
 import { Request, Response } from "express";
 import User from "../../models/userModel";
 import Post from "../../models/postModel";
+import {v2 as cloudinary } from 'cloudinary';
 
 export default async function CreatePost(req:Request,res:Response){
     try{
         
-        const {postedBy,text,image}=req.body;
+        const {text,postedBy,}=req.body;
+        let {image}=req.body;
+      
         
         if(!text && !image){
-           return  res.json({message:"text or image is requird"});
+           return  res.json({error:"text or image is requird"});
 
         }
         if(req.headers["userId"] && !Array.isArray(req.headers["userId"])){
             if(!postedBy){
-              return  res.json({message:"posted by field is required"});
+              return  res.json({error:"Unauthorized"});
     
             }
             const userFromHeader=JSON.parse(req.headers["userId"]);
             const user=await User.findById(postedBy);
             if(!user){
-               return res.json({message:"user not found"});
+               return res.json({error:"user not found"});
             }
             else{
 
                 if(user._id.toString()!=userFromHeader._id.toString()){
-                   return  res.json({message:"You cannot post for other user"});
+                   return  res.json({error:"You cannot post for other user"});
+                }
+                if(image){
+                    const uploadResponse=await cloudinary.uploader.upload(image);
+                    image=uploadResponse.secure_url;
                 }
                 
                 const newPost=new Post({text,image,postedBy});
@@ -44,6 +51,6 @@ export default async function CreatePost(req:Request,res:Response){
 
     }catch(e){
         console.log(e);
-       return  res.json({message:"error in creating post"});
+    return  res.json({error:"error in creating post"});
     }
 }
