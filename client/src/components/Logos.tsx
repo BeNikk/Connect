333b10b8e0ade55eed3e2f26337c37a5de39c8b4
BeNikk <bhattnik442@gmail.com>
2024-05-22 12:00:22 +0530

    likes feature added

--- client/src/components/Logos.tsx
+++ client/src/components/Logos.tsx
@@ -1,31 +1,69 @@
+import userAtom from "@/atoms/userAtom";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useRecoilValue } from "recoil";
 
-const Logos = ({ post }: any) => {
-  const [liked, setLike] = useState(false);
-  const [countLikes, setCountLikes] = useState(1200);
-  console.log(post);
+// interface Post{
+//   postedBy:string;
+//   likes:any[];
+//   replies:any[];
+//   text:string;
+//   image:string;
+
+// }
+// interface post{
+//   post:Post[]
+// }
+
+const Logos = ({ post: post_ }: { post: any }) => {
+  const user = useRecoilValue<any>(userAtom);
+  const [liked, setLiked] = useState(post_.likes.includes(user?._id));
+  const userId = localStorage.getItem("userId") || "";
+  const [post, setPost] = useState(post_);
+  async function handleLikeUnlike() {
+    if (!user) {
+      toast.error("Login required");
+    }
+    try {
+      const res = await fetch(`/api/post/like/${post_._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          userId: userId,
+        },
+      });
+      const data = await res.json();
+      if (data.error) {
+        toast.error(data.error);
+      }
+      if (!liked) {
+        setPost({ ...post, likes: [...post.likes, user._id] });
+      } else {
+        setPost({
+          ...post,
+          likes: post.likes.filter((id: any) => id != user._id),
+        });
+      }
+      setLiked(!liked);
+      console.log(data);
+    } catch (error) {
+      return toast.error("erorr occured");
+    }
+  }
+
+  console.log(post_);
 
   return (
     <div className="flex flex-col justify-start ">
       {post && (
         <div>
           <div className="flex flex-row items-center justify-start gap-4">
-            <div
-              onClick={() => {
-                ``;
-                setLike(!liked);
-              }}
-            >
+            <div onClick={() => {}}>
               <img
                 src={`${liked ? "/heart-filled.svg" : "/heart-gray.svg"}`}
                 alt=""
                 className={`cursor-pointer w-8 h-8`}
-                onClick={() => {
-                  if (!liked) setCountLikes(countLikes + 1);
-                  else {
-                    setCountLikes(countLikes - 1);
-                  }
-                }}
+                onClick={handleLikeUnlike}
               />
             </div>
             <div>
