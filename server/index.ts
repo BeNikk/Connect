bf6dd989a2619d63ab2db2be73911a607ee4823d
BeNikk <bhattnik442@gmail.com>
2024-05-23 13:05:23 +0530

    fixing

--- server/index.ts
+++ server/index.ts
@@ -23,7 +23,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.urlencoded({limit:'50mb',extended:true}));
 connectDB();
-app.use(cors({origin:"https://connect-qegx.onrender.com/"}));
+app.use(cors());
 
 app.use('/api/user',userRouter);
 app.use('/api/post',postRouter);
