ac5a4efa0654e8d4cf9b85256c85fef327f076f3
BeNikk <bhattnik442@gmail.com>
2024-05-23 12:17:45 +0530

    added origin

--- server/index.ts
+++ server/index.ts
@@ -23,7 +23,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.urlencoded({limit:'50mb',extended:true}));
 connectDB();
-app.use(cors());
+app.use(cors({origin:"https://collegeconnect-dusky.vercel.app/"}));
 
 app.use('/api/user',userRouter);
 app.use('/api/post',postRouter);
