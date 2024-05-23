955cc81476043c8ba20ecb159571ecca834a66ee
BeNikk <bhattnik442@gmail.com>
2024-05-23 12:54:36 +0530

    allowed from render

--- server/index.ts
+++ server/index.ts
@@ -23,7 +23,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.urlencoded({limit:'50mb',extended:true}));
 connectDB();
-app.use(cors());
+app.use(cors({origin:"https://connect-qegx.onrender.com/"}));
 
 app.use('/api/user',userRouter);
 app.use('/api/post',postRouter);
